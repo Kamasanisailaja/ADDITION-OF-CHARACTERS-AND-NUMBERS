891aabae4c4163280471b2d6ba6eb2701ea34dd1
Kamasanisailaja <106640335+Kamasanisailaja@users.noreply.github.com>
2023-05-05 07:58:36 +0530

    Add files via upload

---
+++ Alphabets.java
@@ -0,0 +1,14 @@
+//alphabets from ato z
+import java.util.Scanner;
+class Alphabets
+{
+  public static void main(String args[])
+  {
+    char i='8';
+    int b=010;
+      System.out.println(i);
+      System.out.println(b);
+      System.out.println(i+b);
+
+}
+}
